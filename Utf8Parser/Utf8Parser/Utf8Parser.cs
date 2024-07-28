8992f7dd4f946bb051f7c0355c4564159bd5aa0d
anthony.guertin <anthony.guertin@gmail.com>
2024-07-27 19:35:04 -0600

    initial project commit

---
+++ Utf8Parser/Utf8Parser/Utf8Parser.cs
@@ -0,0 +1,39 @@
+﻿namespace Utf8Parser
+{
+    public class UtfParser(string filePath)
+    {
+        private string FilePath { get; set; } = string.IsNullOrEmpty(filePath) ? string.Empty :filePath;
+        public Dictionary<string, string> GetKeyValuePair()
+        {
+            string[] lines = File.ReadAllLines(FilePath);
+            var result = new Dictionary<string, string>();
+            var current = string.Empty;
+
+            foreach (var line in lines)
+            {
+                if (line == "" || line == " ") { continue; }
+
+                var indexEndOfKey = line.IndexOf('=');
+                var key = string.Empty;
+                var value = string.Empty;
+
+                if (!line.Contains('='))
+                {
+                    result[current] += line;
+                    continue;
+                }
+
+                if (indexEndOfKey >= 0 && indexEndOfKey <= line.Length)
+                {
+                    key = line[..indexEndOfKey];
+                    current = key;
+                }
+
+                value = line.Substring(indexEndOfKey + 1);
+                result.Add(key, value);
+                current = key;
+            }
+            return result;
+        }
+    }
+}
